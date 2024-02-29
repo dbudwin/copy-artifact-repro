89d51e6e60f0aea044cf75ef8ec11d47cbff1d00
Drew Budwin <drew@budw.in>
2024-02-29 08:05:57 -0600

    0 test

--- src/__tests__/sum.test.js
+++ src/__tests__/sum.test.js
@@ -1,5 +1,9 @@
 const sum = require('../sum');
 
-test('adds 1 + 2 to equal 3', () => {
+it('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+it('adds 0 + 0 to equal 0', () => {
+  expect(sum(0, 0)).toBe(0);
 });
